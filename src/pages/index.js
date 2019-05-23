7f5725a62ac3e10cef5dc98029fcd961d89178b4
Taylor <15224340+ttstauss@users.noreply.github.com>
2019-05-23 14:56:26 -0600

    clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Taylor, a full-stack developer living in beautiful Provo.</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me.</Link>
-      </p>
     </Layout>
   )
 }
